e1c145bb9fd7698afbb4e5259a7eae90a5c3cfaa
Joyce Cuanag <jcuanag56@gmail.com>
2020-11-19 20:33:04 +0000

    Final Commit

---
+++ ddsbe2/app/Models/User.php
@@ -0,0 +1,16 @@
+<?php
+ namespace App\Models;
+
+ use Illuminate\Database\Eloquent\Model;
+ 
+ class User extends Model{
+    protected $table = 'tbluser2';
+    // column sa table
+    protected $fillable = [
+        'username', 'password'
+    ];
+
+    public $timestamps = false;
+    protected $primaryKey = 'id';
+    protected $hidden = ['password'];
+ }
